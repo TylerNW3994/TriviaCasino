e5e0a611007515bf7c8496ff685d6c9e4bffb54b
TylerNW3994 <TylerNW3994@gmail.com>
2025-02-21 14:33:16 -0500

    Added Swagger

--- TriviaCasinoApi/Program.cs
+++ TriviaCasinoApi/Program.cs
@@ -3,14 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
@@ -21,6 +15,17 @@ builder.Services.AddCors(options =>
                .AllowAnyHeader();
     });
 });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 app.UseCors("AllowReactApp");
 app.UseHttpsRedirection();
