cfc51cdefb21fed346881238361b904104077daa
TylerNW3994 <TylerNW3994@gmail.com>
2025-02-21 13:59:22 -0500

    initial commit

---
+++ TriviaCasinoApi/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+app.UseCors("AllowReactApp");
+app.UseHttpsRedirection();
+
+app.Run();
---
+++ trivia-casino/src/services/triviaService.ts
@@ -0,0 +1,9 @@
+class triviaService {
+    public getSimpleMathProblem(): { num1: number; num2: number; sum: number }  {
+        const num1 = Math.floor(Math.random() * 100);
+        const num2 = Math.floor(Math.random() * 100);
+        const sum = num1 + num2;
+
+        return { num1, num2, sum };
+    }
+}
