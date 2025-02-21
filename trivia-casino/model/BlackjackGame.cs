be1bb897c9db150652a1e05b428f0d0d70024ae7
TylerNW3994 <TylerNW3994@gmail.com>
2025-02-21 14:44:52 -0500

    Determine players scores in blackjack

--- trivia-casino/model/BlackjackGame.cs
+++ trivia-casino/model/BlackjackGame.cs
@@ -1,5 +1,6 @@
 public class BlackjackGame : ACardGame {
     Hand dealerHand = new();
+    Dictionary<string, int> playerScores = new();
 
     string currentPlayer = "";
 
@@ -12,7 +13,7 @@ public class BlackjackGame : ACardGame {
     }
 
     public void NextPlayer() {
-
+        
     }
 
     public override void DetermineWinner() {
@@ -21,7 +22,7 @@ public class BlackjackGame : ACardGame {
 
     public void Hit(string username) {
         Card card = deck.DrawCard();
-
+        DetermineScore(username);
     }
 
     public void SetStatus(GameState status) {
@@ -34,6 +35,32 @@ public class BlackjackGame : ACardGame {
                 deck.DrawCard(),
                 deck.DrawCard()
             };
+            DetermineScore(player.Username);
         }
+    }
+
+    private void DetermineScore(string username) {
+        Hand playerHand = playerHands.get(username);
+
+        if (playerHand == null) {
+            throw new InvalidOperationException("Hand does not exist for player " + username);
+        }
+
+        int score = 0, aceCount = 0;
+        foreach (Card card in playerHand) {
+            score += card.value;
+
+            if (card.rank == "A") {
+                aceCount++;
+            }
+        }
+
+        //In Blackjack, Aces can count for either a 1 or 11.  If the player's score goes over 21, subtract 10 from that score if they have an Ace to count it as a 1.
+        while (score > 21 && aceCount > 0) {
+            score -= 10;
+            aceCount--;
+        }
+
+        playerScores[username] = score;
     }
 }
