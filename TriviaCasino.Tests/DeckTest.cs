54a2ebc8fed9687f28a71c7f9866c488f7791e14
TylerNW3994 <TylerNW3994@gmail.com>
2025-02-24 14:19:00 -0500

    First Test

---
+++ TriviaCasino.Tests/DeckTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using TriviaCasino.Model;
+namespace TriviaCasino.Tests;
+
+public class DeckTest {
+    public void DeckShouldInitialize() {
+        Deck deck = new();
+        Assert.NotNull(deck);
+    }
+
+    public void StandardDeckShouldHave52Cards() {
+        Deck deck = new();
+        deck = deck.CreateStandardDeck();
+
+        Assert.Equal(52, deck.cards.Count);
+        Assert.Equal(5, 2);
+    }
+}
