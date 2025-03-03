32bd05814bd328ba1be0f252cbe3ef8262f47aa8
TylerNW3994 <TylerNW3994@gmail.com>
2025-03-03 13:11:22 -0500

    Create Abstract Play page

---
+++ trivia-casino/src/components/Play.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Blackjack from './Blackjack';
+
+const gameComponents = {
+    blackjack: Blackjack,
+  };
+
+function Play() {
+  const { gameName } = useParams();
+  const normalizedGameName = gameName.toLowerCase();
+  const GameComponent = gameComponents[normalizedGameName];
+
+  return (
+    <div>
+      <h1>Playing {gameName.toUpperCase()}</h1>
+      <GameComponent />
+    </div>
+  );
+}
+
+export default Play;
