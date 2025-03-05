cbe1c7ed3f3af81b465e30a5ecc9afddbc204e91
TylerNW3994 <TylerNW3994@gmail.com>
2025-03-05 12:02:53 -0500

    Added notes and constants

--- trivia-casino/model/BlackjackGame.cs
+++ trivia-casino/model/BlackjackGame.cs
@@ -4,7 +4,7 @@ public class BlackjackGame : ACardGame {
     Dictionary<string, int> playerScores = new();
     private Dictionary<string, Hand> playerSplitHands = new();
 
-    // string currentPlayer = "";
+    public string currentPlayer = "";
 
     public void Initialize() {
         deck.deckType = DeckType.STANDARD;
@@ -17,12 +17,15 @@ public class BlackjackGame : ACardGame {
     public void NextPlayer() {
         // Go through all the players
         // When you get to the end of the list,
-        // Determine the dealer's score,
-        // If the dealer busts, all the players win. 
+        // go to DetemineWinner
     }
 
     public override void DetermineWinner() {
-
+        // Each player can win against the dealer
+        // Players do NOT compete with each other in Blackjack
+        // As such, each player needs to beat the dealer and not bust to be considered a winner
+        // If a player busts, they lose, regardless of Dealer score.
+        // The Dealer does not reveal their cards if all players bust.
     }
 
     public void Hit(string username) {
@@ -35,6 +38,10 @@ public class BlackjackGame : ACardGame {
         else {
             throw new InvalidOperationException("Hand does not exist for player " + username);
         }
+    }
+
+    public void Stand() {
+        // Could implement NextPlayer here instead.
     }
 
     public void SetStatus(GameState status) {
@@ -62,8 +69,8 @@ public class BlackjackGame : ACardGame {
         }
 
         //In Blackjack, Aces can count for either a 1 or 11.  If the player's score goes over 21, subtract 10 from that score if they have an Ace to count it as a 1.
-        while (score > 21 && aceCount > 0) {
-            score -= 10;
+        while (score > BLACKJACK_MAX_SCORE && aceCount > 0) {
+            score -= ACE_SUBTRACTOR;
             aceCount--;
         }
 
@@ -77,4 +84,6 @@ public class BlackjackGame : ACardGame {
             throw new InvalidOperationException("Hand does not exist for player " + username);
         }
     }
+
+    private readonly int BLACKJACK_MAX_SCORE = 21, ACE_SUBTRACTOR = 10;
 }
