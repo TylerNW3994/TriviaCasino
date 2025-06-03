26662c55ddf93859d59e7185bf0724cdab119f38
TylerNW3994 <TylerNW3994@gmail.com>
2025-06-02 21:34:58 -0400

    Abstracted dtos

--- TriviaCasinoApi/model/DTOs/PlayerCardGameDTO.cs
+++ TriviaCasinoApi/model/DTOs/PlayerCardGameDTO.cs
@@ -1,10 +1,9 @@
 namespace TriviaCasinoAPI.Model;
 
-public class PlayerDTO {
-    public string Username { get; set; } = "";
+public class PlayerCardGameDTO : APlayerDTO {
     public List<Card>? Hand { get; set; }
     public int Score { get; set; }
     public int Bet { get; set; }
     public int Chips { get; set; }
-    public string Status { get; set; } = "";
+    // public string Status { get; set; } = "";
 }
