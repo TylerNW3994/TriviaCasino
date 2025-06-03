dc38c1c882b4830234a4f1fb3472d3b4aaa2246d
TylerNW3994 <TylerNW3994@gmail.com>
2025-06-02 20:36:01 -0400

    GameServices

---
+++ TriviaCasinoApi/services/GameService.cs
@@ -0,0 +1,42 @@
+using Microsoft.Identity.Client;
+using TriviaCasinoAPI.Model;
+
+public class GameService {
+    private Dictionary<string, AGame> games { get; set; } = new();
+
+    public AGame CreateNewGame(GameType gameType, string gameId, Player player) {
+        AGame game;
+        switch (gameType) {
+            case GameType.Blackjack:
+                game = new BlackjackGame(gameId);
+                break;
+            // case GameType.Poker:
+            //     game = new PokerGame(gameId);
+            //     break;
+            // case GameType.Slots:
+            //     game = new SlotsGame(gameId);
+            //     break;
+            // Add more game types as needed
+            default:
+                throw new ArgumentException("Invalid game type");
+        }
+        
+        if (player != null) {
+            game.AddPlayer(player);
+            
+            if (game.CurrentPlayer == string.Empty) {
+                game.CurrentPlayer = player.Username;
+            }
+        }
+        
+        game.Initialize();
+        game.StartGame();
+
+        games[gameId] = game;
+        return game;
+    }
+    
+    protected void UpdateGame(string gameId, AGame game) => games[gameId] = game;
+    protected void RemoveGame(string gameId) => games.Remove(gameId);
+    protected AGame GetGame(string gameId) => games.TryGetValue(gameId, out var game) ? game : throw new InvalidOperationException("Game not found");
+}
