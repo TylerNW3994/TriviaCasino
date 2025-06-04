fc9e677102bbf965659e0a40e7563f7d2994a12c
TylerNW3994 <TylerNW3994@gmail.com>
2025-06-04 12:16:12 -0400

    UserProvider to be replaced

--- trivia-casino/src/components/UserProvider.js
+++ trivia-casino/src/components/UserProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 const UserContext = createContext();
 
@@ -12,9 +12,9 @@ export const UserProvider = ({ children }) => {
 
   useEffect(() => {
     if (token) {
-      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      // axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     } else {
-      delete axios.defaults.headers.common['Authorization'];
+      // delete axios.defaults.headers.common['Authorization'];
     }
   }, [token]);
 
